54cd3041c5b2cb04f33eadb96bc7d2a4ede5cdb1
guptan <nguptaa183@gmail.com>
2020-05-19 13:52:04 +0545

    Changes

--- idea/lib/main.dart
+++ idea/lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
 // The main function is the starting point for all our Flutter apps.
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            title: Text("idea"),
-            backgroundColor: Colors.blueGrey[800],
-          ),
-          body: Center(
-            child: Image(image: AssetImage('images/diamond.png')),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[300],
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Container(
+                color: Colors.blueGrey,
+                height: 100,
+                width: 100,
+                child: Text('Container 1'),
+              ),
+              Container(
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+                child: Text('Container 2'),
+              ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
