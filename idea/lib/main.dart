56e482129cdc5b2483cc92d4742c457933649655
guptan <nguptaa183@gmail.com>
2020-05-19 15:39:36 +0545

    Changes

--- idea/lib/main.dart
+++ idea/lib/main.dart
@@ -16,19 +16,30 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image.asset('images/diamond.png', height: 100, width: 100),
-
+                CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: AssetImage('images/avatar.png'),
+                ),
                 SizedBox(height: 1),
-                Text('Nikhil Gupta', style: TextStyle(fontSize:25),),
+                Text(
+                  'Nikhil Gupta',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico',
+                      color: Colors.white),
+                ),
                 RaisedButton(
                   onPressed: () {},
-                  child: const Text('Enabled Button',
-                      style: TextStyle(fontSize: 20)),
+                  child: const Text('nguptaa183@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
                 ),
                 SizedBox(height: 10),
                 RaisedButton(
                   onPressed: () {},
-                  child: const Text('Enabled Button',
+                  child: const Text('+919901408565',
                       style: TextStyle(fontSize: 20)),
                 ),
               ],
