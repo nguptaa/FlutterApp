ea16648245805ba185980afcfb8745d029f767d2
guptan <nguptaa183@gmail.com>
2020-05-21 23:13:07 +0545

    Changes

--- bmi_calculator_flutter/lib/inputPage.dart
+++ bmi_calculator_flutter/lib/inputPage.dart
@@ -15,6 +15,9 @@ class InputPage extends StatefulWidget {
 }
 
 Gender selectedGender;
+int userHeight = 180;
+int userWeight = 74;
+int userAge = 25;
 
 class _InputPageState extends State<InputPage> {
   @override
@@ -73,9 +76,40 @@ class _InputPageState extends State<InputPage> {
                     "Height",
                     style: ktextLabel,
                   ),
-                  Text(
-                    '180',
-                    style: kbigTextLabel,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(
+                        userHeight.toString(),
+                        style: kbigTextLabel,
+                      ),
+                      Text(
+                        'cm',
+                        style: ktextLabel,
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: kactiveCardColor,
+                      thumbColor: Colors.red,
+                      overlayColor: Color(0x29F44326),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 18),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      value: userHeight.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          userHeight = newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
@@ -88,11 +122,85 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReuseableCard(
                     colour: kinkactiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Weight",
+                          style: ktextLabel,
+                        ),
+                        Text(
+                          userWeight.toString(),
+                          style: kbigTextLabel,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RawMaterialBtn(
+                              iconaddsub: Icons.add,
+                              onPress: () {
+                                setState(() {
+                                  userWeight++;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            RawMaterialBtn(
+                              iconaddsub: Icons.remove,
+                              onPress: () {
+                                setState(() {
+                                  userWeight--;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReuseableCard(
                     colour: kinkactiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Age",
+                          style: ktextLabel,
+                        ),
+                        Text(
+                          userAge.toString(),
+                          style: kbigTextLabel,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RawMaterialBtn(
+                              iconaddsub: Icons.add,
+                              onPress: () {
+                                setState(() {
+                                  userAge++;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            RawMaterialBtn(
+                              iconaddsub: Icons.remove,
+                              onPress: () {
+                                setState(() {
+                                  userAge--;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -113,6 +221,30 @@ class _InputPageState extends State<InputPage> {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class RawMaterialBtn extends StatelessWidget {
+  RawMaterialBtn({@required this.iconaddsub, @required this.onPress});
+  final IconData iconaddsub;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      fillColor: Colors.red,
+      elevation: 8.0,
+      constraints: BoxConstraints.tightFor(
+        height: 56.0,
+        width: 56.0,
+      ),
+      onPressed: onPress,
+      shape: CircleBorder(),
+      child: Icon(
+        iconaddsub,
+        color: Colors.white,
       ),
     );
   }
