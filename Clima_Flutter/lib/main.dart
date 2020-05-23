fd0c34b4a1d9a5c032aa9e57574f2a4c2b308e50
guptan <nguptaa183@gmail.com>
2020-05-23 11:58:15 +0545

    Changes

--- Clima_Flutter/lib/main.dart
+++ Clima_Flutter/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  DevicePreview(
+    enabled: false,
+    builder: (context) => MyApp(),
+  ),
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
